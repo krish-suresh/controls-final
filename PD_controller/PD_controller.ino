2cb66f5ed435ed135d882017889ebe6b6923a950
ayushchakra <achakraborty@olin.edu>
2024-05-02 15:15:29 -0400

    pd

--- PD_controller/PD_controller.ino
+++ PD_controller/PD_controller.ino
@@ -1,25 +1,30 @@
-#define USE_ADC_V2 true
-
 #define MOTOR_IN1 11
 #define MOTOR_IN2 3
 
 #include "sensor_data.h"
 
-#define PWM_MID_POINT 170
+#define PWM_MID_POINT 226
 #define VOLTAGE_MID_POINT PWM_MID_POINT / 255 * 12.0
-#define DISTANCE_SETPOINT 0.013
+#define DIST_EM_TO_PM 0.0105
+#define DIST_EM_OFFSET 0.00232
+#define DISTANCE_SETPOINT (DIST_EM_TO_PM - DIST_EM_OFFSET)
 
 #define MAG_LINE_FIT_SLOPE 0.7825
 #define MAG_LINE_FIT_YINT 407
-
-#define K_P 5000.0
-#define K_I 0.0
-#define K_D 0.0
+#define K 1
+#define K_P 100000.0 * K
+#define K_I 0
+#define K_D 0 * K
 
 #define SERIAL_PRINT true
 
+#define SENSOR_BUFFER_LEN (10)
+
 int sensor = 0;
 int sensor_raw = 0;
+int sensor_buffer[SENSOR_BUFFER_LEN] = { 0 };
+int sensor_counter = 0;
+int sensor_average = 0;
 int pwm = PWM_MID_POINT;
 
 unsigned long times[100] = { 0 };
@@ -35,7 +40,7 @@ float v_i = 0;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   pinMode(MOTOR_IN2, OUTPUT);
   pinMode(MOTOR_IN1, OUTPUT);
@@ -67,36 +72,54 @@ void loop() {
   ADCSRA |= 1 << ADIF;
   sensor_raw = ADC;
 
-  sensor = sensor_raw - (pwm * MAG_LINE_FIT_SLOPE + MAG_LINE_FIT_YINT);
-  sensor = (sensor) / 2; 
-  sensor = constrain(sensor, 0, 256);
-  
-  
-  error = SENSOR_LOOKUP_TABLE[sensor] - DISTANCE_SETPOINT;
+  sensor_buffer[sensor_counter] = sensor_raw;
+  // sensor = constrain(sensor, 0, 254);
+
+  sensor_counter++;
+  if (sensor_counter == SENSOR_BUFFER_LEN) {
+    sensor_counter = 0;
+  }
+
+  sensor_average = 0;
+  for (int i = 0; i < SENSOR_BUFFER_LEN; i++) {
+    sensor_average += sensor_buffer[i];
+  }
+  sensor_average /= SENSOR_BUFFER_LEN;
+
+  sensor = sensor_average - (pwm * MAG_LINE_FIT_SLOPE + MAG_LINE_FIT_YINT);
+  // sensor = (sensor) / 2; 
+  sensor = constrain(sensor, 0, 512);
+  if (sensor % 2 == 1) {
+    error = (SENSOR_LOOKUP_TABLE[sensor / 2] + SENSOR_LOOKUP_TABLE[sensor / 2 + 1] ) / 2.0 - DISTANCE_SETPOINT;
+  } else {
+    error = SENSOR_LOOKUP_TABLE[sensor / 2] - DISTANCE_SETPOINT;
+  }
+  // error = SENSOR_LOOKUP_TABLE[sensor] - DISTANCE_SETPOINT;
   
   curTime = micros();
 
   timeDelta = (curTime - prevTime)*1E-6;
   errorSum += timeDelta*error;
 
-  pwm = constrain(PWM_MID_POINT + K_P * error + K_I*errorSum + K_D * (error - prevError)/ timeDelta, 0, 255);
-  if (pwm == 0) {
-    return;
-  }
+  pwm = constrain(PWM_MID_POINT + K_P * error + K_I*errorSum + K_D * (error - prevError)/ timeDelta, 150, 255);
   OCR2A = pwm;
 
 
   if (SERIAL_PRINT && millis() - lastPrintTime > 200) {
-    Serial.print(SENSOR_LOOKUP_TABLE[sensor], 5);
-    Serial.print(",");
+    // Serial.print(SENSOR_LOOKUP_TABLE[sensor], 5);
+    // Serial.print(",");
+    // Serial.print((error - prevError)/ timeDelta, 5);
+    // Serial.print(",");
     Serial.print(error, 5);
     Serial.print(",");
     // Serial.print(K_I*errorSum, 1);
     // Serial.print(",");
     Serial.print(pwm);
     Serial.print(",");
-    Serial.print(v_i);
-    Serial.print(",");
+    // Serial.print(sensor);
+    // Serial.print(",");
+    // Serial.print(sensor_average);
+    // Serial.print(",");
     Serial.println(sensor_raw);
     // Serial.print(",");
     // Serial.print((pwm * MAG_LINE_FIT_SLOPE + MAG_LINE_FIT_YINT));
